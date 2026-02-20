63ea97457de4a24a0688c01ecebfe6bd438ea1bc
RubenGres <ruben.gres@proton.me>
2026-02-20 19:23:05 +0100

    convex shape on objects

--- project/scenes/levels/level_base.gd
+++ project/scenes/levels/level_base.gd
@@ -23,50 +23,23 @@ func _ready() -> void:
 	Manager.is_all_picked = false
 	_create_furniture_collisions()
 	
+
 func _create_furniture_collisions() -> void:
-	# grabe all mesh
 	for node in _furniture.get_children():
 		var meshes: Array[MeshInstance3D] = []
 		_collect_meshes(node, meshes)
 		if meshes.is_empty():
 			continue
 
-		# create static body inside node
 		var static_body := StaticBody3D.new()
 		node.add_child(static_body)
-		
-		# ok là j'avoue j'ai triché pour combiner tout les mesh :((
-		var combined_aabb: AABB
-		var first := true
+
 		for mesh_instance in meshes:
-			# get_aabb() is in local mesh space, transform to global then to node local
-			var local_aabb: AABB = mesh_instance.get_aabb()
-			# Convert all 8 corners through the transform chain
-			var corners: Array[Vector3] = []
-			for x in [local_aabb.position.x, local_aabb.end.x]:
-				for y in [local_aabb.position.y, local_aabb.end.y]:
-					for z in [local_aabb.position.z, local_aabb.end.z]:
-						var world_point := mesh_instance.global_transform * Vector3(x, y, z)
-						var node_point = node.global_transform.affine_inverse() * world_point
-						corners.append(node_point)
-
-			var mesh_aabb := AABB(corners[0], Vector3.ZERO)
-			for corner in corners:
-				mesh_aabb = mesh_aabb.expand(corner)
-
-			if first:
-				combined_aabb = mesh_aabb
-				first = false
-			else:
-				combined_aabb = combined_aabb.merge(mesh_aabb)
-
-		var shape := BoxShape3D.new()
-		shape.size = combined_aabb.size
-		
-		var col_shape := CollisionShape3D.new()
-		col_shape.shape = shape
-		col_shape.position = combined_aabb.get_center()
-		static_body.add_child(col_shape)
+			var shape := mesh_instance.mesh.create_convex_shape()
+			var col_shape := CollisionShape3D.new()
+			col_shape.shape = shape
+			col_shape.transform = node.global_transform.affine_inverse() * mesh_instance.global_transform
+			static_body.add_child(col_shape)
 
 func _collect_meshes(node: Node, result: Array[MeshInstance3D]) -> void:
 	if node is MeshInstance3D:
