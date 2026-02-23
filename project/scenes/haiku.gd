3b02ba28794c060ad9d39d43bf5bba5c2ee4d657
ollig <bbgibi@me.com>
2026-02-23 01:47:56 +0100

    Fix credits
    blablabla la fin elle est mieux

--- project/scenes/haiku.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
-func _process(delta: float) -> void:
-	pass
